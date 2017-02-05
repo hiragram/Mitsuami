0866166b3911fbe5960725052c5287e559ca5015
hiragram <y@yura.me>
2017-02-05 20:10:34 +0900

    Add Xcode project

---
+++ Mitsuami/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mitsuami
+//
+//  Created by yuyahirayama on 2017/02/05.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Mitsuami/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mitsuami
+//
+//  Created by yuyahirayama on 2017/02/05.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
